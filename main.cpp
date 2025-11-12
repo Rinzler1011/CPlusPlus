9eb73878cd4bf8f5ebaa4c29addea71428dc7f8c
Joshua Callary <joshuacallary@miraitechnologies.com.au>
2025-11-12 12:47:03 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define MAX 8
+
+void merge_sort(int arr[MAX], int, int);
+
+int arr[MAX];
+
+int main()
+{
+    std::cout << "Enter 8 unsorted elements:\n";
+    for (int i = 0; i < MAX; i++) {
+        std::cin >> arr[i];
+    }
+
+    merge_sort(arr, 0, MAX - 1);
+
+    std::cout << "Sorted array:\n";
+    for (int i = 0; i < MAX; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
